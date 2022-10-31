9a718df29bd3f2355d3ec5bc5f958dde607e4353
Lenovo <Lenovo@LAPTOP-5TF8U0CA>
2022-10-31 09:49:20 +0300

    ders5

--- deneme/src/deneme/ders5.java
+++ deneme/src/deneme/ders5.java
@@ -1,9 +1,46 @@
 package deneme;
 
+import java.util.Arrays;
+
 public class ders5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	import java.util.Arrays;
+	
+	public class ArrayManipulations{
+		public static void main(String[]args) {
+			double[] doubleArray= {8.4, 9.3, 0.2, 7.9, 3.4};
+			Arrays.sort(doubleArray);
+			System.out.println("%ndoubleArray: ");
+			
+			for(double value: doubleArray) {
+				System.out.println("%.1f", value);
+				
+			}
+			int[]filledIntArray=new int[10];
+			Arrays.fill(filledIntArray, 7);
+			displayArray(filledIntArray,"filledIntArray");
+			
+			int[] intArray= {1, 2, 3, 4, 5, 6};
+			int[] intArrayCopy =new int[intArray.lenght];
+			System.arraycopy(intArray, 0, intArrayCopy, 0, 0);
+			displayArray(intArray,"intArray");
+			displayArray(intArrayCopy,"intArrayCopy%n");
+			
+			boolean b=Array.equals(intArray,intArrayCopy);
+			System.out.printf("%n%nintArray %s intArrayCopy%n",
+					(b ?"==":"!="));
+			
+			
+			b=Array.equals(intArray, filledIntArray);
+			System.out.printf("intArray %s filledIntArray%n",
+					(b ?"==":"!="));
+					
+			
+			
+		}
+	}
+		
 
 	}
 
